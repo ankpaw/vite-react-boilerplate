69c5796df2a4a8b203f123c08fa4d8c1fdc570fa
Ankit Pawar <officiallyhuman@gmail.com>
2024-04-21 01:08:58 +0530

    added welcome greeting

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,4 +1,12 @@
+import { Typography } from "@mui/material";
+
 const Home = () => {
-  return <div>Welcome Home</div>;
+  return (
+    <div>
+      <Typography variant="body1">
+        {`Welcome to ${APP_NAME} v${APP_VERSION}`}
+      </Typography>
+    </div>
+  );
 };
 export default Home;
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+    APP_NAME: JSON.stringify(process.env.npm_package_name),
+  },
   resolve: {
     alias: {
       "@": "/src",
