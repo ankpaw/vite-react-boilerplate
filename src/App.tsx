4b3c070b857c6beb1fe36fd76476cf114443334b
Ankit Pawar <officiallyhuman@gmail.com>
2024-04-20 21:28:30 +0530

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { createContext, useMemo, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import styles from "./App.module.scss";
 import {
   Container,
